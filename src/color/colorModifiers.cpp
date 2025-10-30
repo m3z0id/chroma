fc2479051db549ab2cedccb8b3d0be72eebb488c
vima <bigm3z0id@gmail.com>
2025-10-30 18:29:05 +0100

    Switched fully to std::tuple instead of custom Triple or Quadruple

--- src/color/colorModifiers.cpp
+++ src/color/colorModifiers.cpp
@@ -4,8 +4,6 @@
 #include <cmath>
 
 #include "helpers.h"
-#include "../datatypes/Quadruple.h"
-#include "../datatypes/Triple.h"
 
 void modifyRGB(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>& mods, const bool overflowAllowed) {
     std::array<std::pair<uint8_t*, uint8_t>, 3> channels = {{
@@ -55,40 +53,40 @@ void modifyRGB(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>
 void modifyHSL(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>& mods, const bool overflowAllowed) {
     auto [h, s, l] = RGBToHSL(*r, *g, *b);
 
-    std::array<Triple<float*, float, float>, 3> channels = {{
+    std::array<std::tuple<float*, float, float>, 3> channels = {{
         {&h, h, 360.0f},
         {&s, s, 100.0f},
         {&l, l, 100.0f}
     }};
-    Triple<float*, float, float> invalid = {nullptr, 0.0f, 0.0f};
+    std::tuple<float*, float, float> invalid = {nullptr, 0.0f, 0.0f};
 
     for (const auto& mod : mods) {
         if (mod.operation == Operation::INVALID) continue;
 
         bool modifierNegative = false;
-        Triple<float*, float, float>& editChannel = channels.at(getHSLChannelIndex(mod.editChannel));
-        Triple<float*, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getHSLChannelIndex(mod.modifierChannel)) : invalid;
-        float* currentChannel = editChannel.first;
+        std::tuple<float*, float, float>& editChannel = channels.at(getHSLChannelIndex(mod.editChannel));
+        std::tuple<float*, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getHSLChannelIndex(mod.modifierChannel)) : invalid;
+        float* currentChannel = std::get<0>(editChannel);
 
-        if ((modifierNegative = mod.modifierChannel < 0)) modifierChannel.second = -modifierChannel.second;
+        if ((modifierNegative = mod.modifierChannel < 0)) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
 
         switch (mod.operation) {
             case Operation::ADD:
-                if (overflowAllowed) *currentChannel += modifierChannel.second;
-                else *currentChannel = std::clamp(*currentChannel + modifierChannel.second, 0.0f, modifierChannel.third);
+                if (overflowAllowed) *currentChannel += std::get<1>(modifierChannel);
+                else *currentChannel = std::clamp(*currentChannel + std::get<1>(modifierChannel), 0.0f, std::get<2>(modifierChannel));
 
                 break;
 
             case Operation::INVERT:
-                if (modifierChannel != invalid) *currentChannel = editChannel.third - modifierChannel.second;
-                else *currentChannel = editChannel.third - mod.difference;
+                if (modifierChannel != invalid) *currentChannel = std::get<2>(editChannel) - std::get<1>(modifierChannel);
+                else *currentChannel = std::get<2>(editChannel) - mod.difference;
 
                 break;
 
             case Operation::SET:
-                if (mod.modifierChannel != 0) *currentChannel = modifierChannel.second;
+                if (mod.modifierChannel != 0) *currentChannel = std::get<1>(modifierChannel);
                 else if (overflowAllowed) *currentChannel = mod.difference;
-                else *currentChannel = std::clamp(mod.difference, 0.0f, editChannel.third);
+                else *currentChannel = std::clamp(mod.difference, 0.0f, std::get<2>(editChannel));
 
                 break;
 
@@ -101,7 +99,7 @@ void modifyHSL(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>
             while (*currentChannel < 0.0f) *currentChannel += 360.0f;
             while (*currentChannel >= 360.0f) *currentChannel -= 360.0f;
         }
-        if (modifierNegative) modifierChannel.second = -modifierChannel.second;
+        if (modifierNegative) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
     }
 
     std::tie(*r, *g, *b) = HSLToRGB(h, s, l);
@@ -110,40 +108,40 @@ void modifyHSL(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>
 void modifyOKLAB(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>& mods, const bool overflowAllowed) {
     auto [l, a, b_] = RGBToOKLAB(*r, *g, *b);
 
-    std::array<Quadruple<float*, float, float, float>, 3> channels = {{
+    std::array<std::tuple<float*, float, float, float>, 3> channels = {{
         {&l, l, 0, 1},
         {&a, a, -0.25f, 0.25f},
         {&b_, b_, -0.25f, 0.25f}
     }};
-    Quadruple<float*, float, float, float> invalid = {nullptr, 0.0f, 0.0f, 0.0f};
+    std::tuple<float*, float, float, float> invalid = {nullptr, 0.0f, 0.0f, 0.0f};
 
     for (const auto& mod : mods) {
         if (mod.operation == Operation::INVALID) continue;
 
         bool modifierNegative = false;
-        Quadruple<float*, float, float, float>& editChannel = channels.at(getOKLABChannelIndex(mod.editChannel));
-        Quadruple<float*, float, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getOKLABChannelIndex(std::abs(mod.modifierChannel))) : invalid;
-        float* currentChannel = editChannel.first;
+        std::tuple<float*, float, float, float>& editChannel = channels.at(getOKLABChannelIndex(mod.editChannel));
+        std::tuple<float*, float, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getOKLABChannelIndex(std::abs(mod.modifierChannel))) : invalid;
+        float* currentChannel = std::get<0>(editChannel);
 
-        if ((modifierNegative = mod.modifierChannel < 0)) modifierChannel.second = -modifierChannel.second;
+        if ((modifierNegative = mod.modifierChannel < 0)) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
 
         switch (mod.operation) {
             case Operation::ADD:
-                if (overflowAllowed) *currentChannel += modifierChannel.second;
-                else *currentChannel = std::clamp(*currentChannel + modifierChannel.second, editChannel.third, editChannel.fourth);
+                if (overflowAllowed) *currentChannel += std::get<1>(modifierChannel);
+                else *currentChannel = std::clamp(*currentChannel + std::get<1>(modifierChannel), std::get<2>(editChannel), std::get<3>(editChannel));
 
                 break;
 
             case Operation::INVERT:
-                if (mod.modifierChannel != 0) *currentChannel = editChannel.fourth - modifierChannel.second;
-                else *currentChannel = editChannel.fourth - mod.difference;
+                if (mod.modifierChannel != 0) *currentChannel = std::get<3>(editChannel) - std::get<1>(modifierChannel);
+                else *currentChannel = std::get<3>(editChannel) - mod.difference;
 
                 break;
 
             case Operation::SET:
-                if (mod.modifierChannel != 0) *currentChannel = modifierChannel.second;
+                if (mod.modifierChannel != 0) *currentChannel = std::get<1>(modifierChannel);
                 else if (overflowAllowed) *currentChannel = mod.difference;
-                else *currentChannel = std::clamp(mod.difference, editChannel.third, editChannel.fourth);
+                else *currentChannel = std::clamp(mod.difference, std::get<2>(editChannel), std::get<3>(editChannel));
 
                 break;
 
@@ -151,7 +149,7 @@ void modifyOKLAB(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 
             default:
                 break;
         }
-        if (modifierNegative) modifierChannel.second = -modifierChannel.second;
+        if (modifierNegative) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
     }
 
     std::tie(*r, *g, *b) = OKLABToRGB(l, a, b_);
@@ -160,40 +158,40 @@ void modifyOKLAB(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 
 void modifyOKLCh(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 3>& mods, const bool overflowAllowed) {
     auto [l, c, h] = RGBToOKLCh(*r, *g, *b);
 
-    std::array<Quadruple<float*, float, float, float>, 3> channels = {{
+    std::array<std::tuple<float*, float, float, float>, 3> channels = {{
         {&l, l, 0, 1},
         {&c, c, 0.0f, 0.4f},
         {&h, h, 0.f, 360.0f}
     }};
-    Quadruple<float*, float, float, float> invalid = {nullptr, 0.0f, 0.0f, 0.0f};
+    std::tuple<float*, float, float, float> invalid = {nullptr, 0.0f, 0.0f, 0.0f};
 
     for (const auto& mod : mods) {
         if (mod.operation == Operation::INVALID) continue;
 
         bool modifierNegative = false;
-        Quadruple<float*, float, float, float>& editChannel = channels.at(getOKLChChannelIndex(mod.editChannel));
-        Quadruple<float*, float, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getOKLChChannelIndex(std::abs(mod.modifierChannel))) : invalid;
-        float* currentChannel = editChannel.first;
+        std::tuple<float*, float, float, float>& editChannel = channels.at(getOKLChChannelIndex(mod.editChannel));
+        std::tuple<float*, float, float, float>& modifierChannel = mod.modifierChannel != 0 ? channels.at(getOKLChChannelIndex(std::abs(mod.modifierChannel))) : invalid;
+        float* currentChannel = std::get<0>(editChannel);
 
-        if ((modifierNegative = mod.modifierChannel < 0)) modifierChannel.second = -modifierChannel.second;
+        if ((modifierNegative = mod.modifierChannel < 0)) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
 
         switch (mod.operation) {
             case Operation::ADD:
-                if (overflowAllowed) *currentChannel += modifierChannel.second;
-                else *currentChannel = std::clamp(*currentChannel + modifierChannel.second, editChannel.third, editChannel.fourth);
+                if (overflowAllowed) *currentChannel += std::get<1>(modifierChannel);
+                else *currentChannel = std::clamp(*currentChannel + std::get<1>(modifierChannel), std::get<2>(editChannel), std::get<3>(editChannel));
 
                 break;
 
             case Operation::INVERT:
-                if (mod.modifierChannel != 0) *currentChannel = editChannel.fourth - modifierChannel.second;
-                else *currentChannel = editChannel.fourth - mod.difference;
+                if (mod.modifierChannel != 0) *currentChannel = std::get<3>(editChannel) - std::get<1>(modifierChannel);
+                else *currentChannel = std::get<3>(editChannel) - mod.difference;
 
                 break;
 
             case Operation::SET:
-                if (mod.modifierChannel != 0) *currentChannel = modifierChannel.second;
+                if (mod.modifierChannel != 0) *currentChannel = std::get<1>(modifierChannel);
                 else if (overflowAllowed) *currentChannel = mod.difference;
-                else *currentChannel = std::clamp(mod.difference, editChannel.third, editChannel.fourth);
+                else *currentChannel = std::clamp(mod.difference, std::get<2>(editChannel), std::get<3>(editChannel));
 
                 break;
 
@@ -201,7 +199,7 @@ void modifyOKLCh(uint8_t* r, uint8_t* g, uint8_t* b, const std::array<Modifier, 
             default:
                 break;
         }
-        if (modifierNegative) modifierChannel.second = -modifierChannel.second;
+        if (modifierNegative) std::get<1>(modifierChannel) = -std::get<1>(modifierChannel);
     }
 
     std::tie(*r, *g, *b) = OKLChToRGB(l, c, h);
